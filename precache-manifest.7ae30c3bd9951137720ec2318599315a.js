d186148898a6a63e970f8c9c0590e7cee1089f22
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-30 12:21:44 +0700

    Updates

---
+++ precache-manifest.7ae30c3bd9951137720ec2318599315a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "c7a224b3d208af717bcf",
+    "url": "/todo-react/static/js/main.c7a224b3.chunk.js"
+  },
+  {
+    "revision": "4ca47f3279d7d753fe90",
+    "url": "/todo-react/static/js/1.4ca47f32.chunk.js"
+  },
+  {
+    "revision": "c7a224b3d208af717bcf",
+    "url": "/todo-react/static/css/main.fc12d3e9.chunk.css"
+  },
+  {
+    "revision": "12fd565ec34cada6424a687eee0666c5",
+    "url": "/todo-react/index.html"
+  }
+];
