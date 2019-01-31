4fb933a1a2c5c8426af10e61fe6364375a27f219
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-31 10:47:19 +0700

    link git icon to cource code && add clear all button margins

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
     <ul>
       <li><a href="mailto:winstonsmithcodes@gmail.com?Subject=Hi%20Winston" target="_top"><i className="fas fa-envelope fa-2x"></i></a></li>
       <li><a href="https://www.twitter.com/winstoncodes"><i className="fab fa-twitter fa-2x"></i></a></li>
-      <li><a href="https://www.github.com/winstoncodes"><i className="fab fa-git fa-2x"></i></a></li>
+      <li><a href="https://github.com/winstonCodes/todo-react"><i className="fab fa-git fa-2x"></i></a></li>
     </ul>
   </footer>
 )
